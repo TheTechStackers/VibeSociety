242331968b8e694a4c3466065d7e025b1c59eb99
Vincent Tong <vincent.khang.tong@gmail.com>
2022-08-08 19:33:53 -0500

    server side compiling works

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,23 @@
 import 'reflect-metadata';
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import { HelloResolver } from './resolvers/Hello';
-import cors from 'cors';
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { buildSchema } = require('type-graphql');
+const { HelloResolver } = require('./resolvers/Hello');
+const cors = require('cors');
+
 
 const main = async () => {
   const app = express();
 
   app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}));
+  app.use(express.static("../../client/dist"));
+
+  app.get('/', (req:any, res: any) => {
+    console.log(req);
+    res.send("HELLOOOO");
+  })
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
@@ -19,7 +28,7 @@ const main = async () => {
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
-  app.listen(8080, () => 'server started');
+  app.listen(8080, () => 'the server started');
 };
 
 main();
